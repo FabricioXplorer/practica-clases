03dd6c6990f8737fa37aa142ce443f375df50996
FabricioXplorer <rendonfabricio21@gmail.com>
2023-10-11 10:51:04 -0400

    practica DOM clases

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-
 (()=>{
 const btn = document.querySelector('[data-btn]');
-const createNombre = (e)=>{
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    //console.log(input.value);
-const value=input.value
-const list = document.querySelector('[data-list]');
-const task = document.createElement('li');//aqui creamos nuestro elemento li
-task.classList.add('card');//asigno los estilos de card
-input.value = '';
-const taskContent = document.createElement('div');
-const titleTask = document.createElement('span');
-titleTask.classList.add('task')
-titleTask.innerText= value;//valores 
-taskContent.appendChild(checkComplete());
-taskContent.appendChild(titleTask);//span dentro del div
-    //const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //task.innerHTML=content;
-    task.appendChild(taskContent);//div dentro del li
-    task.appendChild(deleteIcon());
-    list.appendChild(task);//introduce li dentro del ul
+const createTask = (evento)=>{
+        evento.preventDefault();
+        const input = document.querySelector('[data-input]');
+        const input2 = document.querySelector('[data-input2]');
+        console.log(input.value);
+        console.log(input2.value);
+        const value=input.value
+        const value2=input2.value
+        const list = document.querySelector('[data-list]');
+        const task = document.createElement('li');
+        input.value = '';
+const taskDescription = document.createElement('div');
+const taskContent=document.createElement('span');
+task.innerText=value;
+taskContent.innerText=input2;
+taskDescription.innerText=value2;
+taskContent.appendChild(task)
+taskContent.appendChild(taskDescription)
+list.appendChild(taskContent);
 };
-
 btn.addEventListener('click',createTask);
+
 })();
