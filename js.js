747bdd78f079b93a98a875876b045e3744f32f40
FabricioXplorer <rendonfabricio21@gmail.com>
2023-10-12 10:57:30 -0400

    actualizado DOM

---
+++ js.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const h2Element = document.querySelector("h2");
+    h2Element.classList.add("animated"); // "animated" es el nombre de la clase CSS
+  });
+  
--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-(()=>{
+(() => {
 const btn = document.querySelector('[data-btn]');
-const createTask = (evento)=>{
-        evento.preventDefault();
+const createTask = (evento) => {
+evento.preventDefault();
+
         const input = document.querySelector('[data-input]');
         const input2 = document.querySelector('[data-input2]');
-        console.log(input.value);
-        console.log(input2.value);
-        const value=input.value
-        const value2=input2.value
-        const list = document.querySelector('[data-list]');
-        const task = document.createElement('li');
-        input.value = '';
-const taskDescription = document.createElement('div');
-const taskContent=document.createElement('span');
-task.innerText=value;
-taskContent.innerText=input2;
-taskDescription.innerText=value2;
-taskContent.appendChild(task)
-taskContent.appendChild(taskDescription)
-list.appendChild(taskContent);
-};
-btn.addEventListener('click',createTask);
+        const input3 = document.querySelector('[data-input3]');
+      
+        const value = input.value;
+        const value2 = input2.value;
+        const value3 = input3.value;
+      
+          const list = document.querySelector('[data-list]');
+//creacrion de elementos
+        const taskContent = document.createElement('li');
+        const task = document.createElement('p');
+        const taskDescription = document.createElement('p');
+        const taskPrecio = document.createElement('p');
 
+        task.innerText = ("Producto: "+value+"");
+        taskDescription.innerText = ("Descripcion: "+ value2+"");
+        taskPrecio.innerText = ("precio: "+ value3+"");
+//apenChild   
+        taskContent.appendChild(task);
+        taskContent.appendChild(taskDescription);
+        taskContent.appendChild(taskPrecio);
+        list.appendChild(taskContent);
+
+        input.value = '';
+        input2.value = '';
+        input3.value = '';
+        };
+      
+btn.addEventListener('click', createTask);
 })();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+        const h2Element = document.querySelector("h2");
+        h2Element.classList.add("animated"); // "animated" es el nombre de la clase CSS
+      });
+      
